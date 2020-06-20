d7b9ca2b5dbeb18aeef8a99db33c5aca2d3b45bb
diegoA25 <00044919@uca.edu.sv>
2020-06-20 17:50:04 -0600

    Primer avance del juego en si

--- ProyectoPoo/Modelo/ControlArkanoid.Designer.cs
+++ ProyectoPoo/Modelo/ControlArkanoid.Designer.cs
@@ -31,16 +31,39 @@ namespace ProyectoPoo
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ControlArkanoid));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image) (resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(538, 561);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(154, 51);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
             // 
             // ControlArkanoid
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image) (resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Controls.Add(this.pictureBox1);
+            this.DoubleBuffered = true;
             this.Name = "ControlArkanoid";
-            this.Size = new System.Drawing.Size(175, 173);
+            this.Size = new System.Drawing.Size(1217, 657);
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
         }
+
+        private System.Windows.Forms.PictureBox pictureBox1;
 
         #endregion
     }
--- ProyectoPoo/Modelo/ControlArkanoid.cs
+++ ProyectoPoo/Modelo/ControlArkanoid.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using System;
+using System.Windows.Forms;
 
 namespace ProyectoPoo
 {
@@ -7,6 +8,38 @@ namespace ProyectoPoo
         public ControlArkanoid()
         {
             InitializeComponent();
+        }
+        
+        //lo siguiente lee si se presiona la tecla direccional izquierda o derecha pero primero se tiene que dar click en la imagen del player
+
+        public void ControlArkanoid_Load(EventHandler pictureBox1_Click)
+        {
+            pictureBox1.Click += pictureBox1_Click;
+        }
+
+        private PictureBox picselected = null;
+
+        public void pictureBox1_Click(object sender, EventHandler e)
+        {
+            picselected = (PictureBox) sender;
+        }
+
+        protected override bool ProcessCmdKey(ref System.Windows.Forms.Message msg, System.Windows.Forms.Keys keyData)
+        {
+            if (picselected == null) 
+                return base.ProcessCmdKey(ref msg, keyData);
+            switch (keyData)
+            {
+                case Keys.Left:
+                    picselected.Left += 10;
+                    break;
+                case Keys.Right:
+                    //no se por que empezo a fallar el right 
+                    picselected.Right -= 10;
+                    break;
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
         }
     }
 }
