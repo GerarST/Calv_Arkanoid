33c955562c6f5ee4d7299ba493c6c2d99435c6c1
diegoA25 <00044919@uca.edu.sv>
2020-06-21 16:03:17 -0600

    pelota movimiento

--- ProyectoPoo/Modelo/ControlArkanoid.Designer.cs
+++ ProyectoPoo/Modelo/ControlArkanoid.Designer.cs
@@ -31,9 +31,16 @@ namespace ProyectoPoo
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ControlArkanoid));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -49,6 +56,44 @@ namespace ProyectoPoo
             this.pictureBox1.TabStop = false;
             this.pictureBox1.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.pictureBox1_PreviewKeyDown);
             // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(595, 534);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(45, 40);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(57, 0);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 46);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Noto Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(113, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 45);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "X 3";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // ControlArkanoid
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -56,15 +101,24 @@ namespace ProyectoPoo
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackgroundImage = ((System.Drawing.Image) (resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
             this.DoubleBuffered = true;
             this.Name = "ControlArkanoid";
             this.Size = new System.Drawing.Size(1217, 657);
             ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
         }
 
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer timer1;
 
         #endregion
     }
--- ProyectoPoo/Modelo/ControlArkanoid.cs
+++ ProyectoPoo/Modelo/ControlArkanoid.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace ProyectoPoo
@@ -8,24 +9,39 @@ namespace ProyectoPoo
         public ControlArkanoid()
         {
             InitializeComponent();
+            timer1.Interval = 25;
+            timer1.Stop();
         }
         
-        //lo siguiente lee si se presiona la tecla direccional izquierda o derecha pero primero se tiene que dar click en la imagen del player
-
+        //Si se a presionado espacio la pelota comienza a moverse
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (space) {
+            }
+            else
+            {
+                timer1.Start();
+                pictureBox2.Location = new Point(pictureBox2.Location.X+1, pictureBox2.Location.Y-1);
+            }
+        }
+        //El load del arkanoid
         public void ControlArkanoid_Load(EventHandler pictureBox1_Click)
         {
             pictureBox1.Click += pictureBox1_Click;
             picselected = pictureBox1;
         }
 
+        private bool space = false;
         private PictureBox picselected = null;
         private bool ifselected = false;
-
+        
+        //Debe hacerse click en la plataforma primero antes de empezar
         public void pictureBox1_Click(object sender, EventHandler e)
         {
             ifselected = true;
         }
-
+        
+        //mueve la plataforma de izquierda a derecha
         private void pictureBox1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
         {
             if (ifselected){
@@ -40,6 +56,31 @@ namespace ProyectoPoo
                         picselected.Left -= 10;
                         break;
                 }
+            }
+        }
+        
+        //En caso la pelota se salga del limite
+
+        private void pictureBox2_OutofBounds(Object sender, EventHandler e)
+        {
+            int lifes = 3;
+            while (lifes!=0)
+            {
+                if (pictureBox2.Location.Y == 0 && pictureBox2.Location.X <= 1217)
+                {
+                    timer1.Stop();
+                    lifes -= 1;
+                    label1.Text = lifes.ToString();
+                }
+            }
+        }
+        
+        //Detecta cuando se presiona el espacio
+        private void pictureBox2_SpacePress(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                space = true;
             }
         }
     }
